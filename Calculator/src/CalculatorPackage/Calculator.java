627c5b8ef8cad19310aa2a1fd1a0936f4781d5d7
Optio1 <Agentoptio@gmail.com>
2022-09-01 18:27:15 -0500

    Added explanatory text

--- Calculator/src/CalculatorPackage/Calculator.java
+++ Calculator/src/CalculatorPackage/Calculator.java
@@ -2,13 +2,13 @@ package CalculatorPackage;
 
 public class Calculator {
 	public static void main(String[] args) {
-		int height1 = 70;
-		int height2 = 60;
-		int height3 = 78;
-		int height4 = 75;
+		int height1 = 72;
+		int height2 = 61;
+		int height3 = 77;
+		int height4 = 74;
 		int height5 = 72;
 		int sum = height5 + height4 + height3 + height2 + height1;
 		double avg = sum / 5;
-		System.out.println(avg);
+		System.out.println("The average height of these students is " + avg + " inches.");
 	}
 }
