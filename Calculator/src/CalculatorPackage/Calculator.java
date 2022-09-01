3e9a4cf672e281aa247408f5a8ecd56902c919af
Optio1 <Agentoptio@gmail.com>
2022-09-01 18:23:39 -0500

    Updated Calculator to use inches

--- Calculator/src/CalculatorPackage/Calculator.java
+++ Calculator/src/CalculatorPackage/Calculator.java
@@ -2,11 +2,11 @@ package CalculatorPackage;
 
 public class Calculator {
 	public static void main(String[] args) {
-		int height1 = 4;
-		int height2 = 5;
-		int height3 = 6;
-		int height4 = 5;
-		int height5 = 4;
+		int height1 = 70;
+		int height2 = 60;
+		int height3 = 78;
+		int height4 = 75;
+		int height5 = 72;
 		int sum = height5 + height4 + height3 + height2 + height1;
 		double avg = sum / 5;
 		System.out.println(avg);
