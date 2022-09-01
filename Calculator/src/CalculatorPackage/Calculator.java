1976608226e1b3da77ec090d2c12fb6b9a3055af
Optio1 <Agentoptio@gmail.com>
2022-09-01 18:19:24 -0500

    Added calculator module
    Calculates the height in feet from statically set heights.

---
+++ Calculator/src/CalculatorPackage/Calculator.java
@@ -0,0 +1,14 @@
+package CalculatorPackage;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int height1 = 4;
+		int height2 = 5;
+		int height3 = 6;
+		int height4 = 5;
+		int height5 = 4;
+		int sum = height5 + height4 + height3 + height2 + height1;
+		double avg = sum / 5;
+		System.out.println(avg);
+	}
+}
